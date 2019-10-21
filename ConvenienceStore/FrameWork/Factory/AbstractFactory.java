ddeaf30239da835a3658a0a8fa1efd94101ead31
yagotoasa <1664730993@qq.com>
2019-10-21 19:04:01 +0800

    UML-diagram

---
+++ ConvenienceStore/FrameWork/Factory/AbstractFactory.java
@@ -0,0 +1,12 @@
+package Factory;
+
+/**
+ * @className: AbstractFactory
+ * @description: 所有工厂的抽象基类
+ * @author: Shidan Cheng
+ * @date: 10:28 2019/10/21
+ * @version: v1.0
+ */
+public abstract class AbstractFactory{
+
+}
