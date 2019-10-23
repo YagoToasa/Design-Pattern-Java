90f47240c3c32b80c9d8f80a8c98c142a4d53aea
greatroger <yinancheng1999@163.com>
2019-10-23 19:15:09 +0800

    card

---
+++ ConvenienceStore/FrameWork/cards/CardFactory.java
@@ -0,0 +1,14 @@
+package cards;
+
+/**
+ * @className: CardFactory
+ * @author: Yinan Cheng
+ * @description:
+ * @designPattern:
+ * @date: 19:13 2019/10/23
+ */
+public class CardFactory {
+    public void createCard(){
+
+    }
+}
