15c93aa0b67edc4ca64afa4bc34707fbc95e19eb
yagotoasa <1664730993@qq.com>
2019-10-23 22:56:13 +0800

    Delete redundant files

--- ConvenienceStore/TestProgram/FactoryMethodDemo.java
+++ ConvenienceStore/TestProgram/FactoryMethodDemo.java
@@ -12,7 +12,7 @@ import cards.SeniorCardFactory;
  */
 public class FactoryMethodDemo {
     public static void main(String[] args) {
-        System.out.println("START =========================== Factory Method Demo ==========================");
+        System.out.println("START ====================== Factory Method Demo =======================");
         CardFactory ordinaryCardFactory=new OrdinaryCardFactory();
         MembershipCard cardOrdinary=ordinaryCardFactory.getCard("物理全");
         cardOrdinary.createMembershipCard();
@@ -20,6 +20,6 @@ public class FactoryMethodDemo {
         CardFactory seniorCardFactory=new SeniorCardFactory();
         MembershipCard cardSenior=seniorCardFactory.getCard("抖森");
         cardSenior.createMembershipCard();
-        System.out.println("E N D ========================= Factory Method Demo =========================\n");
+        System.out.println("E N D ====================== Factory Method Demo =======================\n");
     }
 }
