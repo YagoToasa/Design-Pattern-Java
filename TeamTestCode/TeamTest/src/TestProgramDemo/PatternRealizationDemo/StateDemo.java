a12601bc9ddac5f9cc64875635c56426a67d3b6c
godsstime <56883233+godsstime@users.noreply.github.com>
2019-11-10 20:16:19 +0800

    Demo
    update

---
+++ TeamTestCode/TeamTest/src/TestProgramDemo/PatternRealizationDemo/StateDemo.java
@@ -0,0 +1,38 @@
+package TestProgramDemo.PatternRealizationDemo;
+
+import kitchen.container.Cabinet;
+import kitchen.container.ContainerIterator;
+import kitchen.ingredient.Ingredient;
+import kitchen.ingredient.IngredientType;
+import kitchen.time.Time;
+/**
+ * @className: StateDemo
+ * @description:
+ * @author: Liwen Zhu
+ * @date: 13:02  2019/11/10
+ * @version: v1.0
+ */
+public class StateDemo {
+    public static void main(String[] args)
+    {
+        System.out.println("START ========================= State Demo =========================\n");
+        Cabinet.getInstance().put(IngredientType.EGG,1);
+        Cabinet.getInstance().put(IngredientType.FLOUR,1);
+        Time.getInstance().update(99);
+        ContainerIterator it = Cabinet.getInstance().Iterator();
+        while(it.hasNext())
+        {
+            Ingredient ingredient = it.next();
+            System.out.println("冰箱中有：【"+ingredient.getName()+"】" + ingredient.getStateName());
+        }
+        Time.getInstance().update(1);
+        it = Cabinet.getInstance().Iterator();
+        while(((ContainerIterator) it).hasNext())
+        {
+            Ingredient ingredient = it.next();
+            System.out.println("冰箱中有：【"+ingredient.getName()+"】"+ ingredient.getStateName());
+        }
+        System.out.println("END ========================= State Demo =========================\n");
+    }
+
+}
